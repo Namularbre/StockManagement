4b1fb2ca11b8c78902bb9db9e63c3b90fe184795
Thomas Sazerat <81816911+Namularbre@users.noreply.github.com>
2025-02-01 15:54:41 +0100

    feat: Added a category listing view

---
+++ StockManagement/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockManagement_Metier.Services;
+
+namespace StockManagement.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.FindAll();
+
+            return View(categories);
+        }
+    }
+}
